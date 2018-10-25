f8694e825a69ac9b92cca2da402ec9c1c9abf82c
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-10-25 18:13:25 +0200

    tests: More MPI/Data stuff

--- tests/test_data.py
+++ tests/test_data.py
@@ -246,7 +246,7 @@ class TestMPIData(object):
         assert np.all(v.data_with_halo._global == 1.)
 
     @pytest.mark.parallel(nprocs=4)
-    def test_global_indexing_basic(self):
+    def test_indexing(self):
         grid = Grid(shape=(4, 4))
         x, y = grid.dimensions
         glb_pos_map = grid.distributor.glb_pos_map
@@ -277,7 +277,7 @@ class TestMPIData(object):
             assert np.all(u.data[:, 2] == [myrank, myrank])
 
     @pytest.mark.parallel(nprocs=4)
-    def test_global_indexing_slicing(self):
+    def test_slicing(self):
         grid = Grid(shape=(4, 4))
         x, y = grid.dimensions
         glb_pos_map = grid.distributor.glb_pos_map
@@ -305,7 +305,7 @@ class TestMPIData(object):
             assert u.data[:2, 2:].size == u.data[2:, :2].size == u.data[:2, :2].size == 0
 
     @pytest.mark.parallel(nprocs=4)
-    def test_global_indexing_after_slicing(self):
+    def test_indexing_in_views(self):
         grid = Grid(shape=(4, 4))
         x, y = grid.dimensions
         glb_pos_map = grid.distributor.glb_pos_map
@@ -319,14 +319,32 @@ class TestMPIData(object):
         assert np.all(view[:] == myrank)
         if LEFT in glb_pos_map[x] and LEFT in glb_pos_map[y]:
             assert view.shape == (1, 1)
+            assert np.all(view == 0.)
+            assert view[0, 0] == 0.
+            assert view[1, 1] is None
+            assert view[1].shape == (0, 1)
         elif LEFT in glb_pos_map[x] and RIGHT in glb_pos_map[y]:
             assert view.shape == (1, 2)
+            assert np.all(view == 1.)
+            assert view[0, 0] is None
+            assert view[1, 1] is None
+            assert view[1].shape == (0, 2)
         elif RIGHT in glb_pos_map[x] and LEFT in glb_pos_map[y]:
             assert view.shape == (2, 1)
+            assert np.all(view == 2.)
+            assert view[0, 0] is None
+            assert view[1, 1] is None
+            assert view[1].shape == (1,)
+            assert np.all(view[1] == 2.)
         else:
             assert view.shape == (2, 2)
+            assert np.all(view == 3.)
+            assert view[0, 0] is None
+            assert view[1, 1] == 3.
+            assert view[1].shape == (2,)
+            assert np.all(view[1] == 3.)
 
-        # Now we further slice into the view
+        # Now we further slice into `view`
         view2 = view[1:, 1:]
         assert np.all(view2[:] == myrank)
         if LEFT in glb_pos_map[x] and LEFT in glb_pos_map[y]:
@@ -338,7 +356,7 @@ class TestMPIData(object):
         else:
             assert view2.shape == (2, 2)
 
-        # Now a change in `view2` by the only rank "sees" it should affect
+        # Now a change in `view2` by the only rank that "sees" it should affect
         # both `view` and `u.data`
         view2[:] += 1
         if RIGHT in glb_pos_map[x] and RIGHT in glb_pos_map[y]:
@@ -438,4 +456,4 @@ def test_oob_guard():
 if __name__ == "__main__":
     from devito import configuration
     configuration['mpi'] = True
-    TestMPIData().test_data_localviews()
+    TestMPIData().test_indexing_in_views()
