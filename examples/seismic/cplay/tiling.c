13ffafe37f107d146c4aa5b2f3c9d4caa1a2d97c
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-03-25 14:29:32 +0000

    Added cilk and proper tiling

--- examples/seismic/cplay/tiling.c
+++ examples/seismic/cplay/tiling.c
@@ -2,8 +2,9 @@
 #include "stdlib.h"
 #include "math.h"
 #include "sys/time.h"
+#include <time.h>
 #include <stdio.h>
-
+#include <cilk/cilk.h>
 
 void print_array(int nrows, int ncols, float **B)
 {
@@ -17,19 +18,80 @@ void print_array(int nrows, int ncols, float **B)
     }
 }
 
-void jacobi_omp(int timesteps, int nrows, int ncols, float **A, float **B)
+void jacobi_omp_par(int timesteps, int nrows, int ncols, float **A, float **B)
 {
   int t, xi, yi;
   for (t = 0; t < timesteps; t++) {
-    //#pragma omp parallel for
+    #pragma omp parallel
+    #pragma omp for
     for (xi = 1; xi < nrows-1; xi++) {
       for (yi = 1; yi < ncols-1; yi++) {
         B[xi][yi] = (A[xi][yi] + A[xi-1][yi] + A[xi+1][yi] + A[xi][yi-1] + A[xi][yi+1])/6;
       }
     }
+    #pragma omp for
     for (xi = 1; xi < nrows-1; xi++) {
-      //#pragma omp simd
       for (yi = 1; yi < ncols-1; yi++) {
+        A[xi][yi] = B[xi][yi];
+      }
+    }
+  }
+}
+
+void jacobi_omp_parsimd(int timesteps, int nrows, int ncols, float **A, float **B)
+{
+  int t, xi, yi;
+  for (t = 0; t < timesteps; t++) {
+    #pragma omp parallel for
+    //#pragma omp simd
+    for (xi = 1; xi < nrows-1; xi++) {
+      #pragma omp simd
+      for (yi = 1; yi < ncols-1; yi++) {
+        B[xi][yi] = (A[xi][yi] + A[xi-1][yi] + A[xi+1][yi] + A[xi][yi-1] + A[xi][yi+1])/6;
+      }
+    }
+    //#pragma omp simd
+    for (xi = 1; xi < nrows-1; xi++) {
+      #pragma omp simd
+      for(yi = 1; yi < ncols-1; yi++) {
+        A[xi][yi] = B[xi][yi];
+      }
+    }
+  }
+}
+
+void jacobi_omp_simd(int timesteps, int nrows, int ncols, float **A, float **B)
+{
+  int t, xi, yi;
+  for (t = 0; t < timesteps; t+=1) {
+    //#pragma omp simd
+    for (xi = 1; xi < nrows-1; xi+=1) {
+      #pragma omp simd
+      for (yi = 1; yi < ncols-1; yi+=1) {
+        B[xi][yi] = (A[xi][yi] + A[xi-1][yi] + A[xi+1][yi] + A[xi][yi-1] + A[xi][yi+1])/6;
+      }
+    }
+    for (xi = 1; xi < nrows-1; xi+=1) {
+      #pragma omp simd
+      for (yi = 1; yi < ncols-1; yi+=1) {
+        A[xi][yi] = B[xi][yi];
+      }
+    }
+  }
+}
+
+void jacobi_cilk(int timesteps, int nrows, int ncols, float **A, float **B)
+{
+  int t, xi, yi;
+  for (t = 0; t < timesteps; t+=1) {
+    //#pragma omp simd
+    cilk_for (xi = 1; xi < nrows-1; xi+=1) {
+      for (yi = 1; yi < ncols-1; yi+=1) {
+        B[xi][yi] = (A[xi][yi] + A[xi-1][yi] + A[xi+1][yi] + A[xi][yi-1] + A[xi][yi+1])/6;
+      }
+    }
+    cilk_for (xi = 1; xi < nrows-1; xi+=1) {
+      for (yi = 1; yi < ncols-1; yi+=1) {
         A[xi][yi] = B[xi][yi];
       }
     }
@@ -39,22 +101,38 @@ void jacobi_omp(int timesteps, int nrows, int ncols, float **A, float **B)
 
 
 
+
+
+void initialize(int nrows, int ncols, float **A_init)
+{
+  // Initialize the matrix
+    int t = 0;
+    int xi = 0;
+    int yi = 0;
+    for (xi = 0; xi < nrows; xi+=1) {
+      for (yi = 0; yi < ncols; yi+=1) {
+        A_init[xi][yi] = 1;
+      }
+    }
+}
+
+
   int main(int argc, const char * argv[]) {
-    int timesteps = 90000;
-    int nrows = 1000;
-    int ncols = 1000;
+    int timesteps = 40;
+    int nrows = 20000;
+    int ncols = 20000;
     int i = 0;
 
     struct timeval t1, t2;
     double elapsedTime;
 
-        float ** A;
+    float ** A;
     A = malloc(nrows * sizeof(float * ));
     if (A == NULL) {
       fprintf(stderr, "out of memory\n");
       return 0;
     }
-    for (i = 0; i < nrows; i++) {
+    for (i = 0; i < nrows; i+=1) {
       A[i] = malloc(ncols * sizeof(float));
       if (A[i] == NULL) {
         fprintf(stderr, "out of memory\n");
@@ -68,7 +146,7 @@ void jacobi_omp(int timesteps, int nrows, int ncols, float **A, float **B)
       fprintf(stderr, "out of memory\n");
       return 0;
     }
-    for (i = 0; i < nrows; i++) {
+    for (i = 0; i < nrows; i+=1) {
       B[i] = malloc(ncols * sizeof(float));
       if (B[i] == NULL) {
         fprintf(stderr, "out of memory\n");
@@ -76,26 +154,74 @@ void jacobi_omp(int timesteps, int nrows, int ncols, float **A, float **B)
       }
     }
 
-// Zero the matrix
-    int t = 0;
-    int xi = 0;
-    int yi = 0;
-    for (xi = 0; xi < nrows; xi++) {
-      for (yi = 0; yi < ncols; yi++) {
-        A[xi][yi] = 1;
-        B[xi][yi] = 1;
-      }
-    }
-// Print the matrix
-//print_array(nrows, ncols, A);
 
+// Jacobi  #pragma omp parallel for
+initialize(nrows, ncols, A); initialize(nrows, ncols, B);
 gettimeofday(&t1, NULL);
-jacobi_omp(timesteps, nrows, ncols, A, B);
+jacobi_omp_par(timesteps, nrows, ncols, A, B);
 gettimeofday(&t2, NULL);
-elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-printf("%f ms.\n ", elapsedTime);
+elapsedTime = (double)(t2.tv_sec-t1.tv_sec)+(double)(t2.tv_usec-t1.tv_usec)/1000000;
+printf("Jacobi OpenMP par-for, Time taken by program is : %f\n",elapsedTime);
 
-//print_array(nrows, ncols, B);
+
+if (nrows < 15) {
+  print_array(nrows, ncols, B);
+}
+
+// Jacobi  #pragma omp simd
+initialize(nrows, ncols, A); initialize(nrows, ncols, B);
+gettimeofday(&t1, NULL);
+jacobi_omp_simd(timesteps, nrows, ncols, A, B);
+gettimeofday(&t2, NULL);
+elapsedTime = (double)(t2.tv_sec-t1.tv_sec)+(double)(t2.tv_usec-t1.tv_usec)/1000000;
+printf("Jacobi OpenMP SIMD, Time taken by program is : %f\n",elapsedTime);
+
+if (nrows < 15) {
+  print_array(nrows, ncols, B);
+}
+// Jacobi  #pragma omp parfor + simd
+initialize(nrows, ncols, A); initialize(nrows, ncols, B);
+gettimeofday(&t1, NULL);
+jacobi_omp_parsimd(timesteps, nrows, ncols, A, B);
+gettimeofday(&t2, NULL);
+elapsedTime = (double)(t2.tv_sec-t1.tv_sec)+(double)(t2.tv_usec-t1.tv_usec)/1000000;
+printf("Jacobi OpenMP parfor SIMD, Time taken by program is : %f\n",elapsedTime);
+
+if (nrows < 15) {
+  print_array(nrows, ncols, B);
+}
+
+
+// Jacobi CILK
+initialize(nrows, ncols, A); initialize(nrows, ncols, B);
+gettimeofday(&t1, NULL);
+jacobi_omp_parsimd(timesteps, nrows, ncols, A, B);
+gettimeofday(&t2, NULL);
+elapsedTime = (double)(t2.tv_sec-t1.tv_sec)+(double)(t2.tv_usec-t1.tv_usec)/1000000;
+printf("Jacobi CILK, Time taken by program is : %f\n",elapsedTime);
+
+if (nrows < 15) {
+  print_array(nrows, ncols, B);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     printf("\n");
