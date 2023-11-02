7a4142e359ed22e31153bd9fc5c192c912efe0f5
George Bisbas <g.bisbas18@imperial.ac.uk>
2023-11-02 13:38:11 +0000

    wip

--- devito/operator/operator.py
+++ devito/operator/operator.py
@@ -265,6 +265,7 @@ class Operator(Callable):
 
         # [LoweredEq] -> [Clusters]
         clusters = cls._lower_clusters(expressions, **kwargs)
+        # import pdb;pdb.set_trace()
 
         # [Clusters] -> ScheduleTree
         stree = cls._lower_stree(clusters, **kwargs)
@@ -337,6 +338,8 @@ class Operator(Callable):
 
         # "True" lowering (indexification, shifting, ...)
         expressions = lower_exprs(expressions, **kwargs)
+
+        # import pdb;pdb.set_trace()
 
         processed = [LoweredEq(i) for i in expressions]
 
